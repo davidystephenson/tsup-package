fa3e5b3d5ea1852ed06f301e300c6231027509b0
David Y. Stephenson <david@davidystephenson.com>
2022-07-19 14:26:27 +0200

    Use dirname

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 
 if (process.argv.length > 2) {
-  const filepath = path.join('.', process.argv[2])
+  const filepath = path.join(__dirname, process.argv[2])
   const packageJson = require(filepath)
 
   function move ({ name, to }) {
