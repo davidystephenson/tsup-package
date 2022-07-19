1445af0fd7b0fb614d21c0136b2d074ed912ccde
David Y. Stephenson <david@davidystephenson.com>
2022-07-19 14:17:59 +0200

    Use argment for package path

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 #! /usr/bin/env node
-const packageJson = require('./package.json')
 const fs = require('fs')
+const path = require('path')
 
-function move ({ name, to }) {
-  if (packageJson.dependencies?.[name] != null) {
-    const { [name]: value, ...without } = packageJson.dependencies
+if (process.argv.length > 2) {
+  const filepath = path.join('.', process.argv[2])
+  const packageJson = require(filepath)
 
-    packageJson.dependencies = without
+  function move ({ name, to }) {
+    if (packageJson.dependencies?.[name] != null) {
+      const { [name]: value, ...without } = packageJson.dependencies
 
-    if (packageJson[to] != null) {
-      packageJson[to][name] = value
-    } else {
-      packageJson[to] = { [name]: value }
+      packageJson.dependencies = without
+
+      if (packageJson[to] != null) {
+        packageJson[to][name] = value
+      } else {
+        packageJson[to] = { [name]: value }
+      }
     }
   }
-}
-move({ name: 'react', to: 'peerDependencies' })
-move({ name: 'react-dom', to: 'peerDependencies' })
-move({ name: 'react-router-dom', to: 'peerDependencies' })
-move({ name: 'react-scripts', to: 'devDependencies' })
+  move({ name: 'react', to: 'peerDependencies' })
+  move({ name: 'react-dom', to: 'peerDependencies' })
+  move({ name: 'react-router-dom', to: 'peerDependencies' })
+  move({ name: 'react-scripts', to: 'devDependencies' })
 
-if (packageJson.files) {
-  if (!packageJson.files.includes('dist')) {
-    packageJson.files.push('dist')
+  if (packageJson.files) {
+    if (!packageJson.files.includes('dist')) {
+      packageJson.files.push('dist')
+    }
+  } else {
+    packageJson.files = ['dist']
   }
-} else {
-  packageJson.files = ['dist']
+
+  packageJson.main = 'dist/App.js'
+  packageJson.types = 'dist/App.d.ts'
+
+  delete packageJson.private
+
+  const json = JSON.stringify(packageJson, null, 2)
+  fs.writeFileSync('./package.json', json)
 }
-
-packageJson.main = 'dist/App.js'
-packageJson.types = 'dist/App.d.ts'
-
-delete packageJson.private
-
-const json = JSON.stringify(packageJson, null, 2)
-fs.writeFileSync('./package.json', json)
